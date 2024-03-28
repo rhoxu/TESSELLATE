adb3e591f102f752b6e09a16e5429a30be6611f3
Hugh Roxburgh <hugh11rox@gmail.com>
2024-03-29 12:20:10 +1300

    Update downloader.py

--- tessellate/downloader.py
+++ tessellate/downloader.py
@@ -65,12 +65,6 @@ def Download_cam_ccd_FFIs(path,sector,cam,ccd,time,lower,upper,number):
         os.mkdir(f'{path}/Cam{cam}')
     if not os.path.exists(newpath):
         os.mkdir(newpath)
-
-    os.chdir(newpath)    
-    print(os.getcwd())
-    print(os.getcwd())
-    print(os.getcwd())
-    print(os.getcwd())
     
     # -- Collates all lines for FFIs of interest -- #
     goodlines = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(_find_lines)(file,cam,ccd,time,lower,upper) for file in filelines)
