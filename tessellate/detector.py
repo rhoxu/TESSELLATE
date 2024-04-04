263b87fa93a8ec78dc5b1f3615d16e7c7db843ed
Ryan Ridden <rri38@Beep-booper.local>
2024-04-04 19:08:12 +1300

    fix

--- tessellate/detector.py
+++ tessellate/detector.py
@@ -469,7 +469,7 @@ class Detector():
         unit = u.electron/ u.s
         light = lk.LightCurve(time=Time(self.time, format='mjd'),flux=(f - np.nanmedian(f))*unit)
         period = light.to_periodogram()
-        ax[4].plot(period.frequency,period.power)
+        ax[4].loglog(period.frequency,period.power)
         ax[4].axvline(period.frequency_at_max_power.value,color='C1',ls='--')
         #plt.axvline(1/14,color='C1',ls='--')
         ax[4].set_title(f'Peak frequency {np.round(period.frequency_at_max_power.value,2)}'+
