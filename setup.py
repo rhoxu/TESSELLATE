7d9c9898ca2912375ae9a7f737da67f32a742f56
Ryan Ridden <rri38@Beep-booper.local>
2024-04-17 11:01:34 +1200

    fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ AUTHOR ='Hugh Roxburgh'
 VERSION = '1.0.0'
 REQUIRED = ['astrocut',
             'photutils>=1.4',
-            'tessreduce @ git+https://github.com/rhoxu/TESSreduce.git' # @b3054d51f2f0993a2ce386d5f2ea7635e5aa2288
+            'tessreduce @ git+https://github.com/rhoxu/TESSreduce.git@b3054d51f2f0993a2ce386d5f2ea7635e5aa2288'
             ]
 
 
