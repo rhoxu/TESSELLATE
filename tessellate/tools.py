3fe23f950d115d3ffc20173b6924b5579cdcc8b0
Hugh Roxburgh <roxburghhugh@gmail.com>
2024-07-12 00:20:03 +0800

    overwriting functionality

--- tessellate/tools.py
+++ tessellate/tools.py
@@ -7,7 +7,7 @@ def _remove_ffis(data_path,sector,n,cams,ccds,cuts):
     for cam in cams:
         for ccd in ccds:
             os.chdir(f'{data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}')
-            os.system('rm -r image_files')
+            os.system('rm -r -f image_files')
 
     os.chdir(home_path)
 
@@ -17,9 +17,9 @@ def _remove_cubes(data_path,sector,n,cams,ccds,cuts):
     for cam in cams:
         for ccd in ccds:
             os.chdir(f'{data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}')
-            os.system(f'rm sector{sector}_cam{cam}_ccd{ccd}_cube.fits')
-            os.system(f'rm sector{sector}_cam{cam}_ccd{ccd}_wcs.fits')
-            os.system(f'rm cubed.txt')
+            os.system(f'rm -f sector{sector}_cam{cam}_ccd{ccd}_cube.fits')
+            os.system(f'rm -f sector{sector}_cam{cam}_ccd{ccd}_wcs.fits')
+            os.system(f'rm -f cubed.txt')
 
     os.chdir(home_path)
 
@@ -28,7 +28,7 @@ def _remove_cuts(data_path,sector,n,cams,ccds,cuts):
     for cam in cams:
         for ccd in ccds:
             for cut in cuts:
-                os.system(f'rm -r {data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}/Cut{cut}of{n**2}')
+                os.system(f'rm -r -f {data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}/Cut{cut}of{n**2}')
 
 def _remove_reductions(data_path,sector,n,cams,ccds,cuts):
 
@@ -37,12 +37,12 @@ def _remove_reductions(data_path,sector,n,cams,ccds,cuts):
         for ccd in ccds:
             for cut in cuts:
                 os.chdir(f'{data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}/Cut{cut}of{n**2}')
-                os.system(f'rm *.npy')
-                os.system(f'rm reduced.txt')
-                os.system(f'rm detected_events.csv')
-                os.system(f'rm detected_sources.csv')
-                os.system('rm -r figs')
-                os.system('rm -r lcs')    
+                os.system(f'rm -f *.npy')
+                os.system(f'rm -f reduced.txt')
+                os.system(f'rm -f detected_events.csv')
+                os.system(f'rm -f detected_sources.csv')
+                os.system('rm -r -f figs')
+                os.system('rm -r -f lcs')    
 
     os.chdir(home_path)
 
@@ -53,10 +53,10 @@ def _remove_search(data_path,sector,n,cams,ccds,cuts):
         for ccd in ccds:
             for cut in cuts:
                 os.chdir(f'{data_path}/Sector{sector}/Cam{cam}/Ccd{ccd}/Cut{cut}of{n**2}')
-                os.system(f'rm detected_events.csv')
-                os.system(f'rm detected_sources.csv')
-                os.system('rm -r figs')
-                os.system('rm -r lcs')    
+                os.system(f'rm -f detected_events.csv')
+                os.system(f'rm -f detected_sources.csv')
+                os.system('rm -r -f figs')
+                os.system('rm -r -f lcs')    
 
     os.chdir(home_path)
 
