55fc6ca0d93996aaff1683e0834c8ef9a5e90713
Hugh Roxburgh <roxburghhugh@gmail.com>
2024-11-01 12:56:58 +0800

    Update external_photometry.py

--- tessellate/external_photometry.py
+++ tessellate/external_photometry.py
@@ -199,14 +199,14 @@ def _DESI_phot(ra,dec,size):
 def event_cutout(coords,size=50,phot=None):
 
     if phot is None:
-
+        phot = 'DESI'
         fig,wcs,size = _DESI_phot(coords[0],coords[1],size)
         if fig is None:
             if coords[1] > -10:
                 phot = 'PS1'
             else:
                 phot = 'SkyMapper'
-        
+
     if phot == 'PS1':
         fig,wcs,size = _Panstarrs_phot(coords[0],coords[1],size)
 
