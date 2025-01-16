9bdeebd3f208bc94a83ab0f0767d6e71a56259b8
Hugh Roxburgh <roxburghhugh@gmail.com>
2025-01-17 00:46:56 +1300

    Update external_photometry.py

--- tessellate/external_photometry.py
+++ tessellate/external_photometry.py
@@ -178,12 +178,14 @@ def _DESI_phot(ra,dec,size):
     urlFITS = f"http://legacysurvey.org/viewer/cutout.fits?ra={ra}&dec={dec}&size={size}"
     urlIM = f"http://legacysurvey.org/viewer/cutout.jpg?ra={ra}&dec={dec}&size={size}"
 
+    print(urlFITS)
+
     response = requests.get(urlIM)
     if response.status_code == 200:
         image = Image.open(BytesIO(requests.get(urlIM).content))
         
         # try:
-        hdulist = fits.open(BytesIO(requests.get(urlFITS).content),ignore_missing_simple=True)
+        hdulist = fits.open(BytesIO(requests.get(urlFITS).content))
         hdu = hdulist[0]
         wcs = WCS(hdu.header)
         wcs = wcs.dropaxis(2)
