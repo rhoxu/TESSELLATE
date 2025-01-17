ab1e0cc887147b7c716aa76a2eb8168a7b0a0c83
Hugh Roxburgh <roxburghhugh@gmail.com>
2025-01-17 15:48:25 +1300

    Update detector.py

--- tessellate/detector.py
+++ tessellate/detector.py
@@ -1271,7 +1271,7 @@ class Detector():
     def plot_source(self,cut,id,event='seperate',savename=None,save_path='.',
                     star_bin=True,period_bin=True,type_bin=True,objectid_bin='auto',
                     include_periodogram=False,latex=True,period_power_limit=10,
-                    asteroid_check=False,zoo_mode=True,save_lc=True,external_phot=True,latex=True):
+                    asteroid_check=False,zoo_mode=True,save_lc=True,external_phot=True):
         if latex:
             plt.rc('text', usetex=latex)
             plt.rc('font', family='serif')
