f42cb3b9d4d311d18a5d238bb66d86958bfc84ec
CheerfulUser <ryanridden@gmail.com>
2025-01-24 14:35:12 +1100

    added astro-datalab requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ REQUIRED = ['astrocut',
             'tessreduce @ git+https://github.com/CheerfulUser/TESSreduce.git',#git+https://github.com/rhoxu/TESSreduce.git',#@b3054d51f2f0993a2ce386d5f2ea7635e5aa2288'
             'sourcedetect @ git+https://github.com/andrewmoore73/SourceDetect.git',
             'shapely',
-            'mastcasjobs'
+            'mastcasjobs',
+            'astro-datalab'
             ]
 
 
