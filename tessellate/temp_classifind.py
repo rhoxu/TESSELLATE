5aee7d89b51e9bfbc21e0c3c56cd49c5dc3e1ec0
Hugh Roxburgh <roxburghhugh@gmail.com>
2025-07-22 12:33:31 +0800

    Update temp_classifind.py

--- tessellate/temp_classifind.py
+++ tessellate/temp_classifind.py
@@ -409,17 +409,18 @@ class get_dataset:
                 period = periods[i]
             except:
                 period = None
-            try:
-                if len(df) == 0:
-                    df = pd.DataFrame([get_features(time=time,flux=flux,period=period,scaled=scaled).features])
-                else:
-                    try:
-                        df = pd.concat([df, pd.DataFrame([get_features(time=time,flux=flux,period=period,scaled=scaled).features])], ignore_index=True)
-                        df = df.sort_index()
-                    except:
-                        pass
-            except:
-                pass
+        # try:
+            if len(df) == 0:
+                df = pd.DataFrame([get_features(time=time,flux=flux,period=period,scaled=scaled).features])
+                print(df)
+            else:
+                try:
+                    df = pd.concat([df, pd.DataFrame([get_features(time=time,flux=flux,period=period,scaled=scaled).features])], ignore_index=True)
+                    df = df.sort_index()
+                except:
+                    pass
+        # except:
+        #     pass
         self.table = df
 
 
@@ -666,15 +667,15 @@ class classifind:
             self.train_and_test()
         self.table = get_dataset(self.lcs,periods=self.periods,scaled=self.scaled).table
 
-        print('\n')
-        print(f'LCs: {self.lcs}')
-        print('\n')
+        # print('\n')
+        # print(f'LCs: {self.lcs}')
+        # print('\n')
 
-        print(f'Periods: {self.periods}')
-        print('\n')
+        # print(f'Periods: {self.periods}')
+        # print('\n')
 
-        print(f'Scaled: {self.scaled}')
-        print('\n')
+        # print(f'Scaled: {self.scaled}')
+        # print('\n')
 
         # print(self.table)
         self.predict(self.model,self.table,self.classes)
