2aaf6c73cb511d7f194a007e463c5a3ec4b935e1
Hugh Roxburgh <roxburghhugh@gmail.com>
2026-02-24 11:40:16 +0800

    Update downloader.py

--- tessellate/downloader.py
+++ tessellate/downloader.py
@@ -76,6 +76,8 @@ def Download_cam_ccd_FFIs(path,sector,cam,ccd,time,lower,upper,number):
     if number == 'all':
         inds = np.linspace(0,len(goodlines)-1,len(goodlines)).astype(int)
         Parallel(n_jobs=multiprocessing.cpu_count())(delayed(_download_line)(goodlines[ind],path,cam,ccd) for ind in tqdm(inds, position=0, leave=True,dynamic_ncols=False,ascii=True))
+    elif type(number) == int:
+        _download_line(goodlines[number],path,cam,ccd)
     elif number > 10:
         inds = np.linspace(0,number-1,number).astype(int)
         Parallel(n_jobs=multiprocessing.cpu_count())(delayed(_download_line)(goodlines[ind],path,cam,ccd) for ind in tqdm(inds, position=0, leave=True,dynamic_ncols=False,ascii=True))
