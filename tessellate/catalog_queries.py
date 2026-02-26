770ed232374623a4420b8b6c65b325fdcecb2ad9
Hugh Roxburgh <roxburghhugh@gmail.com>
2026-02-26 10:11:00 +0800

    bugfix

--- tessellate/catalog_queries.py
+++ tessellate/catalog_queries.py
@@ -124,7 +124,7 @@ def Get_Gaia(ra,dec,size,wcsObj,magnitude_limit = 18, Offset = 10,verbose=False)
         # result = result.iloc[good_coords]
         coords = wcsObj.all_world2pix(ras,decs, 0) ## TODO, is origin supposed to be zero or one?
 
-    print(coords)
+    coords = np.array(coords).T
 
     source = result['Source'].values
     Gmag = result['Gmag'].values
