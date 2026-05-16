22c161130d5a1af586335b1a7c9fb5565c2bc02d
Hugh Roxburgh <roxburghhugh@gmail.com>
2026-05-16 17:00:30 +0800

    bugfix

--- tessellate/tessellate.py
+++ tessellate/tessellate.py
@@ -2331,7 +2331,7 @@ python {self.working_path}/detection_scripts/S{self.sector}C{cam}C{ccd}C{cut}_sc
                         print('\n')
                         self._cut_reduce(cam=cam,ccd=ccd,cut=cut,time=result)
 
-                    elif job_status != 'RUNNING':
+                    elif job_status not in ['RUNNING','PENDING']:
                         e = f'Job {job_id} for reduction of Cam {cam} CCD {ccd} Cut {cut} has unexpected status: {job_status}\n'
                         raise ValueError(e)
 
