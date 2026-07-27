c1048fd402ed374148b0494a2409c57f2c31664b
Hugh Roxburgh <roxburghhugh@gmail.com>
2026-07-27 12:47:40 +0800

    bugfix

--- tessellate/detector.py
+++ tessellate/detector.py
@@ -2223,7 +2223,7 @@ class Detector():
             from .localisation import simulate_cut_psf_fitting
 
             print('-------Simulating PSF fit accuracy (see progress in errors log file)-------',flush=True)
-            simulate_cut_psf_fitting(self.data_path,self.sector,self.cam,self.ccd,cut,n=self.n,nfits=100000,nMedians=10)
+            simulate_cut_psf_fitting(self.prf_path,self.sector,self.cam,self.ccd,cut,n=self.n,nfits=100000,nMedians=10)
             print('\n')
 
         # -- self.events contains all individual events, grouped by time and space -- #  
